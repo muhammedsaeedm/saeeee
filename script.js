6d86476d1fb65fb8581913337589a4b2d3077213
muhammedsaeedm <115515438+muhammedsaeedm@users.noreply.github.com>
2024-09-12 11:34:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('map-link').addEventListener('click', function(event) {
 
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     const googleMapsMobileUrl = 'geo:0,0?q=Vrindavanam+Convention+Center+Palode';
-    const googleMapsDesktopUrl = 'https://www.google.com/maps?q=Vrindavanam+Convention+Center+Palode';
+    const googleMapsDesktopUrl = 'https://www.google.com/maps/place/Vrindavanam+Convention+Centre/@8.7295557,77.0279169,17z/data=!3m1!4b1!4m6!3m5!1s0x3b05cf6e7ad35cad:0xbce81987a4447f11!8m2!3d8.7295557!4d77.0279169!16s%2Fg%2F11ggr0hnmj?entry=ttu&g_ep=EgoyMDI0MDkwOS4wIKXMDSoASAFQAw%3D%3D';
 
     const conventionNameElement = document.getElementById('convention-name');
     conventionNameElement.textContent = "Vrindavanam Convention Center (Aster Hall), Palode";
